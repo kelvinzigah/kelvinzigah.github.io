c7996d3fa07690a4b88760829759618c228104f3
Kelvin Zigah <149982601+kelvinzigah@users.noreply.github.com>
2026-02-27 10:33:16 -0500

    aded deploy.yml and editted config.mjs to deploy using github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https:/kelvinzigah.github.io',
+  base: '/kelvinzigah.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
