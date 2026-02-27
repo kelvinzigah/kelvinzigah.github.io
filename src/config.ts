03c89b890cea60601e59404216f8216e87d77858
Kelvin Zigah <149982601+kelvinzigah@users.noreply.github.com>
2026-02-27 10:17:39 -0500

    Update config.ts with name

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Kelvin Zigah",
+  title: "Electrical Engineering Undergrad",
+  description: "Portfolio website of Kelvin Zigah",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "zigmystro@gmail.com",
+    linkedin: "https://www.linkedin.com/in/kelvinzigah/",
+    github: "https://github.com/kelvinzigah",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
